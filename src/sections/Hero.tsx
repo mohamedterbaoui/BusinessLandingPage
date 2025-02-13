72ed5b2c5b51ec6834db912d2750b88e8890e03a
mohamed <m.terbaoui7@gmail.com>
2025-02-12 19:50:24 -0500

    Added Button component for reusability

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,11 @@
 export const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <section>
+      <div className="container">
+        <h1>AdsPower</h1>
+        <p>Boost your numbers and profitability with astounding ads and marketing campaigns</p>
+        <button>Get in touch</button>
+      </div>
+    </section>
+  );
 };
