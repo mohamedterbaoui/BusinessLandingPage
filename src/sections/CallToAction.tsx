887bcfda28ca73bf6a3bb2cf83be486ec7d5b48c
mohamed <m.terbaoui7@gmail.com>
2025-02-13 23:08:42 -0500

    added animation to call to action section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,23 +1,68 @@
+"use client";
 import { Button } from "@/components/Button";
 import starsBg from "@/assets/stars.png";
 import gridLines from "@/assets/grid-lines.png";
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
+import { RefObject, useEffect, useRef } from "react";
+
+const useRelativeMousePosition = (to: RefObject<HTMLElement>) => {
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  const updateMousePosition = (event: MouseEvent) => {
+    if (!to.current) return;
+    const { top, left } = to.current.getBoundingClientRect();
+    mouseX.set(event.x - left);
+    mouseY.set(event.y - top);
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", updateMousePosition);
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+  return [mouseX, mouseY];
+};
 
 export const CallToAction = () => {
+  const borderedDivRef = useRef<HTMLDivElement>(null);
+
+  const [mouseX, mouseY] = useRelativeMousePosition(borderedDivRef);
+  const maskImage = useMotionTemplate`radial-gradient(50% 50% at ${mouseX}px ${mouseY}px,black,transparent)`;
+
   return (
     <section className="py-20 md:py-24">
       <div className="container">
-        <div
-          className="py-24 border border-white/15 rounded-xl overflow-hidden relative"
+        <motion.div
+          ref={borderedDivRef}
+          animate={{
+            backgroundPositionX: starsBg.width,
+          }}
+          transition={{
+            repeat: Infinity,
+            duration: 50,
+            ease: "linear",
+          }}
+          className="py-24 border border-white/15 rounded-xl overflow-hidden relative group"
           style={{
             backgroundImage: `url(${starsBg.src})`,
           }}
         >
           <div
-            className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]"
+            className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)] group-hover:opacity-0 transition duration-700"
             style={{
               backgroundImage: `url(${gridLines.src})`,
             }}
           ></div>
+          <motion.div
+            className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay opacity-0 group-hover:opacity-100 transition duration-700"
+            style={{
+              maskImage,
+              backgroundImage: `url(${gridLines.src})`,
+            }}
+          ></motion.div>
           <div className="relative">
             <h2 className="text-5xl md:text-6xl max-w-sm mx-auto font-medium text-center tracking-tighter">
               AI-driver SEO for everyone
@@ -29,7 +74,7 @@ export const CallToAction = () => {
               <Button>Start Today</Button>
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,9 +1,12 @@
+"use client";
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
 
-import Image from 'next/image';
+import { motion } from "framer-motion";
+
+import Image from "next/image";
 
 const testimonials = [
   {
@@ -40,33 +43,52 @@ export const Testimonials = () => {
           Beyond Expectations
         </h2>
         <p className="text-lg md:text-xl text-white/70 text-center tracking-tight mt-5 max-w-sm mx-auto">
-          Our revolutionary AI SEO tools have transformed our clients' strategies.
+          Our revolutionary AI SEO tools have transformed our clients'
+          strategies.
         </p>
-        <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <div className="flex gap-5">
-            {testimonials.map(testimonial => (
-                <div key={testimonial.name} 
-                className="flex-none max-w-xs md:max-w-md p-6 md:p-10 bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] rounded-xl border border-white/15 flex-col justify-start items-start inline-flex">
-                  <h3 className="text-lg md:text-2xl tracking-tight">
-                    {testimonial.text}
-                  </h3>
-                  <div className="flex items-center gap-3 mt-5">
-                    <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg">
-                      <Image src={testimonial.avatarImg} 
-                      alt="avatar" 
-                      className="h-11 w-11 rounded-lg grayscale "/>
-                    </div>
-                    <div className="">
-                      <h4 className="">{testimonial.name}</h4>
-                      <h5 className="text-white/50 text-sm">{testimonial.title}</h5>
-                    </div>
+        <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <motion.div
+            initial={{
+              translateX: "-50%",
+            }}
+            animate={{
+              translateX: "0",
+            }}
+            transition={{
+              repeat: Infinity,
+              ease: "linear",
+              duration: 30,
+            }}
+            className="flex gap-5 pr-5 flex-none"
+          >
+            {[...testimonials, ...testimonials].map((testimonial) => (
+              <div
+                key={testimonial.name}
+                className="flex-none max-w-xs md:max-w-md p-6 md:p-10 bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] rounded-xl border border-white/15 flex-col justify-start items-start inline-flex"
+              >
+                <h3 className="text-lg md:text-2xl tracking-tight">
+                  {testimonial.text}
+                </h3>
+                <div className="flex items-center gap-3 mt-5">
+                  <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg">
+                    <Image
+                      src={testimonial.avatarImg}
+                      alt="avatar"
+                      className="h-11 w-11 rounded-lg grayscale "
+                    />
+                  </div>
+                  <div className="">
+                    <h4 className="">{testimonial.name}</h4>
+                    <h5 className="text-white/50 text-sm">
+                      {testimonial.title}
+                    </h5>
                   </div>
                 </div>
+              </div>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
   );
-
 };
