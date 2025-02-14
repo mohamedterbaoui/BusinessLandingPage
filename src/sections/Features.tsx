2ff72b6104c5294563b743e9e9c9d363db098bde
mohamed <m.terbaoui7@gmail.com>
2025-02-13 22:27:49 -0500

    added animation on featuretabs

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,8 +1,19 @@
 "use client";
-import { DotLottiePlayer } from '@dotlottie/react-player';
-import Image from 'next/image';
-import productImage from '@/assets/product-image.png';
-
+import {
+  DotLottieCommonPlayer,
+  DotLottiePlayer,
+} from "@dotlottie/react-player";
+import Image from "next/image";
+import productImage from "@/assets/product-image.png";
+import { ComponentPropsWithoutRef, useEffect, useRef, useState } from "react";
+import {
+  animate,
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  ValueAnimationOptions,
+  ValueAnimationTransition,
+} from "framer-motion";
 
 const tabs = [
   {
@@ -31,33 +42,114 @@ const tabs = [
   },
 ];
 
-export const Features = () => {
+const FeatureTab = (
+  props: (typeof tabs)[number] &
+    ComponentPropsWithoutRef<"div"> & { selected: boolean }
+) => {
+  const dotLottieRef = useRef<DotLottieCommonPlayer>(null);
+  const tabRef = useRef<HTMLDivElement>(null);
+
+  const xPercentage = useMotionValue(0);
+  const yPercentage = useMotionValue(0);
+
+  const maskImage = useMotionTemplate`radial-gradient(80px 80px at ${xPercentage}% ${yPercentage}%, black, transparent)`;
+
+  useEffect(() => {
+    if (!tabRef.current || !props.selected) return;
+
+    xPercentage.set(0);
+    yPercentage.set(0);
+
+    const { height, width } = tabRef.current?.getBoundingClientRect();
+    const circumference = height * 2 + width * 2;
+
+    const times = [
+      0,
+      width / circumference,
+      (width + height) / circumference,
+      (width * 2 + height) / circumference,
+      1,
+    ];
+
+    const options: ValueAnimationTransition = {
+      times,
+      duration: 4,
+      repeat: Infinity,
+      ease: "linear",
+      repeatType: "loop",
+    };
+    animate(xPercentage, [0, 100, 100, 0, 0], options);
+    animate(yPercentage, [0, 0, 100, 100, 0], options);
+  }, [props.selected]);
+
+  const handleTabHover = () => {
+    if (dotLottieRef.current === null) return;
+    dotLottieRef.current.seek(0);
+    dotLottieRef.current.play();
+  };
   return (
-    <section className='py-20 md:py-24'>
+    <div
+      ref={tabRef}
+      onMouseEnter={handleTabHover}
+      className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1 relative"
+      onClick={props.onClick}
+    >
+      {props.selected && (
+        <motion.div
+          style={{
+            maskImage,
+          }}
+          className="absolute inset-0 -m-px border border-[#A369FF] rounded-xl"
+        ></motion.div>
+      )}
+
+      <div className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
+        <DotLottiePlayer
+          ref={dotLottieRef}
+          src={props.icon}
+          className="h-5 w-5"
+          autoplay
+        />
+      </div>
+      <div className="font-medium">{props.title}</div>
+      {props.isNew && (
+        <div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">
+          new
+        </div>
+      )}
+    </div>
+  );
+};
+
+export const Features = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+  return (
+    <section className="py-20 md:py-24">
       <div className="container">
-        <h2 className='text-5xl md:text-6xl font-medium text-center tracking-tighter'>
+        <h2 className="text-5xl md:text-6xl font-medium text-center tracking-tighter">
           Elevate your SEO efforts.
-          </h2>
-        <p className='text-white/70 text-lg md:text-xl max-w-2xl mx-auto tracking-tight text-center mt-5'>
-          From small startups to large enterprises, 
-          our AI-driver tool has revolutionized the way businesses approach SEO.
+        </h2>
+        <p className="text-white/70 text-lg md:text-xl max-w-2xl mx-auto tracking-tight text-center mt-5">
+          From small startups to large enterprises, our AI-driver tool has
+          revolutionized the way businesses approach SEO.
         </p>
-        <div className='mt-10 flex flex-col lg:flex-row gap-3'>
-          {tabs.map(tab => (
-          <div key={tab.title} className='border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1'>
-            <div className='h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center'>
-              <DotLottiePlayer src={tab.icon} className='h-5 w-5' autoplay/>
-            </div>
-            <div className='font-medium'>{tab.title}</div>
-            {tab.isNew && <div className='text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold'>new</div>}
-          </div>
+        <div className="mt-10 flex flex-col lg:flex-row gap-3">
+          {tabs.map((tab, tabIndex) => (
+            <FeatureTab
+              {...tab}
+              selected={selectedTab === tabIndex}
+              onClick={() => setSelectedTab(tabIndex)}
+              key={tab.title}
+            />
           ))}
         </div>
-        <div className='border border-white/20 p-2.5 rounded-xl mt-3'>
-          <div className='aspect-video bg-cover border border-white/20 rouned-lg' style={{
-            backgroundImage: `url(${productImage.src})`
-          }}>
-          </div>
+        <div className="border border-white/20 p-2.5 rounded-xl mt-3">
+          <div
+            className="aspect-video bg-cover border border-white/20 rouned-lg"
+            style={{
+              backgroundImage: `url(${productImage.src})`,
+            }}
+          ></div>
         </div>
       </div>
     </section>
